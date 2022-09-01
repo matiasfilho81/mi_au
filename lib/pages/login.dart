6e10999e0be846d8fe1811a4a6281ae220a570ac
Tiago Bueno <tiagobuenogarcia@gmail.com>
2022-09-01 11:32:10 -0300

    meu segundo commit

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,1 +1,12 @@
 //irei colocar o codigo aqui
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
