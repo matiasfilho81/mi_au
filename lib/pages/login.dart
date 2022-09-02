1c3aafebbb9a50aea50706902be61d8f78059b8d
Tiago Bueno <tiagobuenogarcia@gmail.com>
2022-09-01 21:30:23 -0300

    teste

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,12 +1,63 @@
-//irei colocar o codigo aqui
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+      padding: const EdgeInsets.only(top: 60, left: 40, right: 40,),
+      color: Colors.amber,
+      child: ListView(
+        children: [
+          SizedBox(
+            width: 128,
+            height: 128,
+            child: Image.asset('assets/images/logo.png'),
+          ),
+          const  SizedBox(height: 20,),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: const InputDecoration(
+              labelText: "E-mail",
+              labelStyle: TextStyle(
+                color: Colors.black38,
+                fontWeight: FontWeight.w400,
+                fontSize: 20,
+              ),
+            ),
+            style: const TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 10,),
+          TextFormField(
+            keyboardType: TextInputType.text,
+            obscureText: true,
+            decoration:const InputDecoration(
+              labelText: "senha",
+              labelStyle: TextStyle(
+                color: Colors.black38,
+                fontWeight: FontWeight.w400,
+                fontSize: 20, 
+                ),
+            ),
+            style: const TextStyle(fontSize: 20),
+            ),
+            Container(
+              height: 40,
+              alignment: Alignment.centerRight,
+              child: const TextButton(
+                child: Text (
+                   "Você é novo? Cadastre-se",
+               
+                ),
+                
+               ),
+            ),
+        ],
+      ),
+      ),
+    );
     
   }
 }
