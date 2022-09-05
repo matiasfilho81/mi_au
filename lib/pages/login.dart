2506cb430114f8011f26048454d4c2b7fa3e5728
Tiago Bueno <tiagobuenogarcia@gmail.com>
2022-09-05 12:57:44 -0300

    wip- tela de login e imagens

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,4 +1,3 @@
-//teste
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
@@ -8,59 +7,106 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-      padding: const EdgeInsets.only(top: 60, left: 40, right: 40,),
-      color: Colors.amber,
-      child: ListView(
-        children: [
-          SizedBox(
-            width: 128,
-            height: 128,
-            child: Image.asset('assets/images/logo.png'),
-          ),
-          const  SizedBox(height: 20,),
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-              labelText: "E-mail",
-              labelStyle: TextStyle(
-                color: Colors.black38,
-                fontWeight: FontWeight.w400,
-                fontSize: 20,
-              ),
+        padding: const EdgeInsets.only(
+          top: 60,
+          left: 40,
+           right: 40,
+           ),
+        color: Colors.amber,
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              width: 128,
+              height: 128,
+              child: Image.asset("assets/images/logoo.png"),
             ),
-            style: const TextStyle(fontSize: 20),
-          ),
-          const SizedBox(height: 10,),
-          TextFormField(
-            keyboardType: TextInputType.text,
-            obscureText: true,
-            decoration:const InputDecoration(
-              labelText: "senha",
-              labelStyle: TextStyle(
-                color: Colors.black38,
-                fontWeight: FontWeight.w400,
-                fontSize: 20, 
+            const SizedBox(
+              height: 20,
+            ),
+            TextFormField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(
+                labelText: "E-mail",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: const TextStyle(
+                fontSize: 20,
                 ),
             ),
-            style: const TextStyle(fontSize: 20),
+             const SizedBox(
+              height: 10,
+            ),
+            TextFormField(
+              //autofocus:true,
+              keyboardType: TextInputType.text,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: "Senha",
+                labelStyle: TextStyle(
+                  color: Colors.black38,
+                  fontWeight: FontWeight.w400,
+                  fontSize: 20,
+                ),
+              ),
+              style: const TextStyle(fontSize: 20),
             ),
             Container(
               height: 40,
               alignment: Alignment.centerLeft,
-
-              child: const ElevatedButton(
-                child: Text (
-                   "Você é novo? Cadastre-se",
-               
+              child:  ElevatedButton(
+                child: const Text(
+                  "Você é novo? Cadastre-se",                  
                 ),
-                onPressed: () {},
-                
-               ),
-
+                onPressed:() {},
+              ),
             ),
-            SizedBox()
-        ],
-      ),
+            const SizedBox(
+              height: 40,
+            ),
+            Container(
+              height: 60,
+              alignment: Alignment.centerLeft,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 1],
+                  colors: [
+                    Color.fromARGB(255, 109, 36, 245),
+                     Color.fromARGB(255, 5, 78, 25),
+                  ],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5),
+                  ),
+              ),
+              child: SizedBox.expand(
+                child:  ElevatedButton(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Text(
+                        "Entrar",
+                        style:  TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center,
+                        ),
+                      
+                    ],
+                  ),
+                  onPressed: () => {},
+                   ),
+              ),
+            )
+            ],
+        ),
       ),
     );
     
