adf8ae8c310265335aa34bbf26a16ee41e7ee116
José Matias Lemes Filho <matias@dilettasolutions.com>
2022-09-08 20:22:18 -0300

    configuração da responsividade

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:mi_au/pages/login.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import '/pages/login.dart';
+import '/utils/app_responsive/app_responsive_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +14,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        systemNavigationBarColor: Colors.transparent, // navigation bar color
+        statusBarColor: Colors.transparent, // status bar color
+        statusBarIconBrightness: Brightness.dark, // status bar icons' color
+        systemNavigationBarIconBrightness: Brightness.dark, //navigation bar icons' color
+      ),
+    );
+
+    return MaterialApp.router(
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const LoginPage(),
+      builder: (context, child) {
+        return  const AppResponsiveWidget(child: LoginPage());   
+      },
+      routeInformationParser: Modular.routeInformationParser,
+      routerDelegate: Modular.routerDelegate,
     );
+  }
+}
+
+class MyBehavior {
+  Widget buildMyBehavior(
+    BuildContext context,
+    Widget child,
+    AxisDirection axisDirection,
+  ) {
+    return child;
   }
 }
