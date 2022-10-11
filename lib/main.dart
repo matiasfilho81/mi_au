277f05f6de27e872214ef27d87b1fe9679851951
José Matias Lemes Filho <matiasfilho@gmail.com>
2022-10-11 12:47:40 -0300

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:mi_au/pages/home.dart';
 import 'package:mi_au/pages/login.dart';
 
 import 'utils/app_responsive/app_responsive_widget.dart';
